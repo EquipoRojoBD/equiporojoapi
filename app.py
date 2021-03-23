28c0d7fd80710f00de922db577bca71337018b2d
DR-MATTH <mmarinv@est.emi.edu.bo>
2021-03-23 17:45:23 -0400

    get_personas

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import flask
-from flask import request
+from flask import request, jsonify
+import psycopg2
 
 app = flask.Flask(__name__)
 
+conn = psycopg2.connect(host = "tuffi.db.elephantsql.com", database = "ewayqilc", user = "ewayqilc", password = "c_3fHeJM-wn6440Q4ZU0F9hf7dyDu9ba")
+cur = conn.cursor()
 
-@app.route('/', methods=['GET'])
-def home():
-    return '<h1>probando la API, Equipo Rojo<h1/>'
+@app.route('/personas', methods=['GET'])
+def get_personas():
+    personas = []
+    sql = "select * from persona"
+    cur.execute(sql)
+    for row in cur.fetchall():
+        personas.append({
+            'id': row[0],
+            'nss': row[1],
+            'nombre': row[2],
+            'telefono': row[3]
+        })
+    return jsonify(personas)
